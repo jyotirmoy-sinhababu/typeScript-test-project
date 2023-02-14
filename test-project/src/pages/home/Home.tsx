d865fa576c3e4edd4840227fa6f2e0b18b8b087d
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-15 01:09:58 +0530

    some changes made

--- test-project/src/pages/home/Home.tsx
+++ test-project/src/pages/home/Home.tsx
@@ -4,6 +4,11 @@ import InputField from '../../components/InputField';
 
 import { useState } from 'react';
 
+interface data {
+  data: object;
+  id: number;
+}
+
 const Home = () => {
   const [inputFieldData, setInputFieldData] = useState<any>([]);
 
@@ -13,14 +18,19 @@ const Home = () => {
         ...inputFieldData,
         { id: Math.floor(Math.random() * 1000), data: Object },
       ]);
-      console.log(data);
     }
   };
+  console.log(inputFieldData);
 
   return (
-    <div>
-      <InputField handleSubmit={handleSubmit} />
-    </div>
+    <>
+      {inputFieldData?.map((item: any) => {
+        return <div key={item.id}></div>;
+      })}
+      <div>
+        <InputField handleSubmit={handleSubmit} />
+      </div>
+    </>
   );
 };
 
