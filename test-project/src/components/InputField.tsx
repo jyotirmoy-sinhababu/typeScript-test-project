bf66b75e37de6d58ce8f11f74979a7a8c69468a2
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-02-14 23:31:33 +0530

    on change and handleSubmit btn created

--- test-project/src/components/InputField.tsx
+++ test-project/src/components/InputField.tsx
@@ -2,22 +2,37 @@ import React from 'react';
 
 import { useState } from 'react';
 
-const InputField = () => {
+interface props {
+  inputFieldData: Array<object>;
+  setInputFieldData: React.Dispatch<any>;
+}
+
+const InputField = ({ setInputFieldData, inputFieldData }: props) => {
   const [todo, setTodo] = useState<object>({});
 
   const handleChange = (e: any) => {
     setTodo({ ...(todo as object), [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setInputFieldData(...inputFieldData, todo);
+  };
+
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <input
           type='text'
           placeholder='write your note'
           onChange={handleChange}
           name='todo'
         />
+        <button type='submit'>Add</button>
       </form>
     </div>
   );
--- test-project/src/pages/home/Home.tsx
+++ test-project/src/pages/home/Home.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import InputField from '../../components/InputField';
 
+import { useState } from 'react';
+
 const Home = () => {
+  const [inputFieldData, setInputFieldData] = useState<any>([]);
+
   return (
     <div>
       <InputField />
